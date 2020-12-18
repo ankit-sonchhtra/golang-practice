46cd84bbb465a396841a7a66f8e8feed1144c72b
ankit-cognologix <ankit.sonchhtra@cognologix.com>
2020-12-18 10:38:23 +0530

    xml parsing example

---
+++ xmlparsing/XmlParsingDemo.go
@@ -0,0 +1,48 @@
+// package main
+
+// import "fmt"
+// import "io/ioutil"
+// import "encoding/xml"
+// import "os"
+
+// type Users struct {
+// 	XMLName xml.Name `xml:"users"`
+// 	Users   []User	 `xml:"user"` 
+// }
+
+// type User struct {
+// 	XMLName xml.Name `xml:"user"`
+// 	Type 	string	 `xml:"type,attr"` 
+// 	Name	string	 `xml:"name"` 
+// 	Social  Social   `xml:"social"` 
+// }
+
+// type Social struct {
+// 	XMLName 	xml.Name `xml:"social"`
+// 	Facebook 	string	 `xml:"facebook"` 
+// 	Twitter 	string	 `xml:"twitter"`
+// 	Youtube 	string	 `xml:"youtube"`
+// }
+
+// func main() {
+// 	fmt.Println("Welcome to xml demo")
+
+// 	xmlFile, err := os.Open("users.xml")
+
+// 	if(err != nil) {
+// 		fmt.Println(err)
+// 	}
+
+// 	fmt.Println("File opened successfully.")
+
+// 	defer xmlFile.Close()
+
+// 	byteValue,_ := ioutil.ReadAll(xmlFile)
+
+// 	var users Users
+
+// 	xml.Unmarshal(byteValue, &users)
+
+// 	fmt.Println(users.Users[0])
+	
+// }
